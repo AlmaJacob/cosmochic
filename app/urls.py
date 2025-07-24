7d08f6173eed9713c9b75913a068b6e58451c1b1
Alma <almajacob7@gmail.com>
2025-07-24 10:54:44 +0530

    index name

--- app/urls.py
+++ app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.user_home),
+    path('',views.user_home, name='user_home'),
     # --------shop-------------------
     path('login',views.cosmetic_login),
     path('logout',views.cosmetic_logout),
